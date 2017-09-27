6d29182619fc635fa8067be1fb3f2cca4a412a60
Stephane Larocque <stephl001@gmail.com>
2017-09-27 08:06:34 -0400

    Create ProgrammableDrone class
    This class will represent a programmable drone, that is, a drone that
    can execute a program sequence.
    Also created unit test for initialization.

---
+++ Minesweeper.Tests/ProgrammableDroneTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Minesweeper.Tests
+{
+    public sealed class ProgrammableDroneTests
+    {
+        [Fact]
+        public void TestProgrammableDroneInitialization()
+        {
+            var pd = new ProgrammableDrone(50, 50, 10, 10, FacingDirection.North, "");
+            pd.Program.Should().Be(string.Empty);
+        }
+
+        [Fact]
+        public void TestProgrammableDroneInitializationBadProgram()
+        {
+            Action act = () => new ProgrammableDrone(50, 50, 10, 10, FacingDirection.North, "xyz");
+            act.ShouldThrow<ArgumentException>().And.ParamName.Should().Be("program");
+        }
+
+        [Fact]
+        public void TestProgrammableDroneInitializationValidProgram()
+        {
+            Action act = () => new ProgrammableDrone(50, 50, 10, 10, FacingDirection.North, "<*>*<<<>*>*<<*>*<*>>*<***");
+            act.ShouldNotThrow<ArgumentException>();
+        }
+    }
+}
---
+++ Minesweeper/ProgrammableDrone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Minesweeper
+{
+    public class ProgrammableDrone : Drone
+    {
+        public ProgrammableDrone(int xlimit, int ylimit, int x, int y, FacingDirection dir, string program)
+            : base(xlimit, ylimit, x, y, dir)
+        {
+            if (program.Any(IsInvalidProgramInput))
+                throw new ArgumentException("Only <,> and * are accepted as program input.", nameof(program));
+
+            Program = program;
+        }
+
+        private static bool IsInvalidProgramInput(char c)
+        {
+            return ((c != '*') && (c != '<') && (c != '>'));
+        }
+
+        public string Program { get; }
+    }
+}

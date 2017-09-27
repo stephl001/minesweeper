a08477182fac6d94f32141acb51aad225764520d
Stephane Larocque <stephl001@gmail.com>
2017-09-26 23:14:52 -0400

    Create Drone class
    Added code for initialization with argument check and unit tests.

---
+++ Minesweeper.Tests/DroneTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Minesweeper.Tests
+{
+    public sealed class DroneTests
+    {
+        [Fact]
+        public void TestDroneInitialization()
+        {
+            var drone = new Drone(10, 5, 0, 0, FacingDirection.East);
+            drone.XLimit.Should().Be(10);
+            drone.YLimit.Should().Be(5);
+            drone.X.Should().Be(0);
+            drone.Y.Should().Be(0);
+            drone.Direction.Should().Be(FacingDirection.East);
+
+            drone = new Drone(10, 20, 5, 8, FacingDirection.North);
+            drone.XLimit.Should().Be(10);
+            drone.YLimit.Should().Be(20);
+            drone.X.Should().Be(5);
+            drone.Y.Should().Be(8);
+            drone.Direction.Should().Be(FacingDirection.North);
+        }
+
+        [Fact]
+        public void TestDroneBadInitialization()
+        {
+            Action act = () => new Drone(-1, 10, 0, 0);
+            act.ShouldThrow<ArgumentOutOfRangeException>().And.ParamName.Should().Be("xlimit");
+            act = () => new Drone(10, -1, 0, 0);
+            act.ShouldThrow<ArgumentOutOfRangeException>().And.ParamName.Should().Be("ylimit");
+            act = () => new Drone(10, 10, -1, 0);
+            act.ShouldThrow<ArgumentOutOfRangeException>()
+               .And.Message.Should().Match("X position must be greater or equal to 0 and less or equal to XLimit.*Parameter name: x");
+            act = () => new Drone(10, 10, 11, 0);
+            act.ShouldThrow<ArgumentOutOfRangeException>()
+               .And.Message.Should().Match("X position must be greater or equal to 0 and less or equal to XLimit.*Parameter name: x");
+            act = () => new Drone(10, 10, 0, -1);
+            act.ShouldThrow<ArgumentOutOfRangeException>()
+               .And.Message.Should().Match("Y position must be greater or equal to 0 and less or equal to YLimit.*Parameter name: y");
+            act = () => new Drone(10, 10, 10, 11);
+            act.ShouldThrow<ArgumentOutOfRangeException>()
+               .And.Message.Should().Match("Y position must be greater or equal to 0 and less or equal to YLimit.*Parameter name: y");
+        }
+
+        [Fact]
+        public void TestDroneLimitInitialization()
+        {
+            Action act = () => new Drone(100, 250, 0, 0);
+            act.ShouldNotThrow<ArgumentOutOfRangeException>();
+            act = () => new Drone(100, 250, 100, 250);
+            act.ShouldNotThrow<ArgumentOutOfRangeException>();
+        }
+    }
+}
---
+++ Minesweeper/Drone.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Minesweeper
+{
+    public enum FacingDirection
+    {
+        East,
+        West,
+        North,
+        South
+    };
+
+    public class Drone
+    {
+        public Drone(int xlimit, int ylimit, int x, int y)
+            : this(xlimit, ylimit, x, y, FacingDirection.East)
+        {
+        }
+
+        public Drone(int xlimit, int ylimit, int x, int y, FacingDirection dir)
+        {
+            if (xlimit < 0)
+                throw new ArgumentOutOfRangeException(nameof(xlimit), "A limit cannot be negative.");
+            if (ylimit < 0)
+                throw new ArgumentOutOfRangeException(nameof(ylimit), "A limit cannot be negative.");
+            if ((x < 0) || (x > xlimit))
+                throw new ArgumentOutOfRangeException(nameof(x), "X position must be greater or equal to 0 and less or equal to XLimit.");
+            if ((y < 0) || (y > ylimit))
+                throw new ArgumentOutOfRangeException(nameof(y), "Y position must be greater or equal to 0 and less or equal to YLimit.");
+            XLimit = xlimit;
+            YLimit = ylimit;
+            X = x;
+            Y = y;
+            Direction = dir;
+        }
+
+        public int XLimit { get; }
+        public int YLimit { get; }
+
+        public int X { get; }
+        public int Y { get; }
+
+        public FacingDirection Direction { get; }
+    }
+}

77a095a0673a9d17dc40dd8fac7c23b942f3f8a7
Stephane Larocque <stephl001@gmail.com>
2017-09-28 22:06:44 -0400

    Create Battlefield
    The battlefield class will run a program for each configured drone that
    was specified in an input. It will write the result to a generic
    StreamWriter.

---
+++ Minesweeper.Tests/BattleFieldTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Minesweeper.Tests.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Minesweeper.Tests
+{
+    public sealed class BattleFieldTests
+    {
+        [Fact]
+        public void TestExecution()
+        {
+            RunSimulation("Sample.txt", "SampleExpectedOutput.txt").Should().BeTrue();
+            RunSimulation("Sample2.txt", "Sample2ExpectedOutput.txt").Should().BeTrue();
+        }
+
+        private bool RunSimulation(string sourceInputResource, string expectedOutputResource)
+        {
+            using (StreamReader reader = new ResourceLineReader(sourceInputResource))
+            {
+                var battlefieldInput = new BattlefieldInput(reader);
+                var battlefield = new Battlefield(battlefieldInput);
+
+                using (var memStream = new MemoryStream())
+                {
+                    using (StreamWriter sw = new StreamWriter(memStream))
+                    {
+                        battlefield.Execute(sw);
+                        sw.Flush();
+                        memStream.Seek(0L, SeekOrigin.Begin);
+
+                        bool identical = IsOutputIdenticalToExpected(memStream, expectedOutputResource);
+                        return identical;
+                    }
+                }
+            }
+        }
+
+        private bool IsOutputIdenticalToExpected(MemoryStream memStream, string resourceName)
+        {
+            using (StreamReader source = new StreamReader(memStream, Encoding.UTF8, false, 1024, true))
+            {
+                using (StreamReader target = new ResourceLineReader(resourceName))
+                {
+                    while (!source.EndOfStream && !target.EndOfStream)
+                    {
+                        if (source.ReadLine() != target.ReadLine())
+                            return false;
+                    }
+
+                    return (source.EndOfStream && target.EndOfStream);
+                }
+            }
+        }
+    }
+}
---
+++ Minesweeper/Battlefield.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Minesweeper
+{
+    public sealed class Battlefield
+    {
+        private readonly BattlefieldInput _input;
+
+        public Battlefield(BattlefieldInput input)
+        {
+            _input = input;
+        }
+
+        public void Execute(StreamWriter sw)
+        {
+            foreach (ProgrammableDrone pd in _input.GetProgrammableDrones())
+            {
+                Drone finalDrone = pd.ExecuteProgram();
+                sw.WriteLine(finalDrone.ToString());
+            }
+        }
+    }
+}
